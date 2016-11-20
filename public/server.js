a095058dddc2aae86b89084c27de264e5b10d1ae
Igor Popikov <titbm@yandex.ru>
2016-11-20 06:09:40 +0300

    Updated server.js for localhost & added some changes to About & Examples pages

--- public/server.js
+++ public/server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 // Open Weather Api допускает использование только http://-запросов (не https://)
 SERVER.use(function(request, response, next) {
-  if (request.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (request.headers['x-forwarded-proto'] === 'https') {
     response.redirect('http://' + request.hostname + request.url);
+  } else {
+    next();
   }
 });
 
