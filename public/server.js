2b7bc5c712db1bef7ee07d58f972496ad9011847
Igor Popikov <titbm@yandex.ru>
2016-11-20 05:34:28 +0300

    Added changes to server.js for Heroku

--- public/server.js
+++ public/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
-const SERVER = express();
+var SERVER = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Open Weather Api допускает использование только http://-запросов (не https://)
+SERVER.use(function(request, response, next) {
+  if (request.headers['x-forwarder-proto'] === 'http') {
+    next();
+  } else {
+    response.redirect('http://' + request.hostname + request.url);
+  }
+});
 
 SERVER.use(express.static('public'));
 
-SERVER.listen (3000, function() {
-  console.log ('Server is up on port: 3000');
+SERVER.listen (PORT, function() {
+  console.log ('Server is up on port: ' + PORT);
 });
